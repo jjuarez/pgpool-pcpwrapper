69a85dbf4ebc1f506d36469e658e8cab427831ef
Javier Juarez <jjuarez@tuenti.com>
2015-09-07 09:58:10 +0200

    Some tests

--- spec/pgpool/wrapper/node_info_spec.rb
+++ spec/pgpool/wrapper/node_info_spec.rb
@@ -2,78 +2,71 @@ require 'spec_helper'
 
 describe PGPool::Wrapper::NodeInfo do
 
+  before do
+    @node_info_id     = 1
+    @node_info_host   = 'backend1'
+    @node_info_port   = 5432
+    @node_info_status = 0
+    @node_info_weight = 0.5
+    @node_info        = PGPool::Wrapper::NodeInfo.build_from_raw_data(
+        @node_info_id, 
+        "#{@node_info_host} #{@node_info_port} #{@node_info_status} #{@node_info_weight}")
+  end
+
   it 'can be built from raw data' do
 
-    node_info_id     = 1
-    node_info_host   = 'backend1'
-    node_info_port   = 5432
-    node_info_status = 0
-    node_info_weight = 0.5
-    node_info        = PGPool::Wrapper::NodeInfo.build_from_raw_data(node_info_id, "#{node_info_host} #{node_info_port} #{node_info_status} #{node_info_weight}")
+    expect(@node_info.class).to be PGPool::Wrapper::NodeInfo
 
-    expect(node_info.class).to be PGPool::Wrapper::NodeInfo
+    expect(@node_info.id.class).to be(Fixnum)
+    expect(@node_info.id).to       eq(@node_info_id)
 
-    expect(node_info.id.class).to be Fixnum
-    expect(node_info.id).to eq(node_info_id)
+    expect(@node_info.host.class).to be(String)
+    expect(@node_info.host).to       eq(@node_info_host)
 
-    expect(node_info.host.class).to be String
-    expect(node_info.host).to eq(node_info_host)
+    expect(@node_info.port.class).to be(Fixnum)
+    expect(@node_info.port).to       eq(@node_info_port)
 
-    expect(node_info.port.class).to be Fixnum
-    expect(node_info.port).to eq(node_info_port)
+    expect(@node_info.status.class).to be(Fixnum)
+    expect(@node_info.status).to       eq(@node_info_status)
 
-    expect(node_info.status.class).to be Fixnum
-    expect(node_info.status).to eq(node_info_status)
-
-    expect(node_info.weight.class).to be Float
-    expect(node_info.weight).to eq(node_info_weight)
+    expect(@node_info.weight.class).to be(Float)
+    expect(@node_info.weight).to       eq(@node_info_weight)
   end
 
   it 'can be built from parameters' do
 
-    node_info_id     = 1
-    node_info_host   = 'backend1'
-    node_info_port   = 5432
-    node_info_status = 0
-    node_info_weight = 0.5
-    node_info        = PGPool::Wrapper::NodeInfo.new(node_info_id, node_info_host, node_info_port, node_info_status, node_info_weight)
+    expect(@node_info.class).to be(PGPool::Wrapper::NodeInfo)
 
-    expect(node_info.class).to be PGPool::Wrapper::NodeInfo
+    expect(@node_info.id.class).to be(Fixnum)
+    expect(@node_info.id).to       eq(@node_info_id)
 
-    expect(node_info.id.class).to be Fixnum
-    expect(node_info.id).to eq(node_info_id)
+    expect(@node_info.host.class).to be(String)
+    expect(@node_info.host).to       eq(@node_info_host)
 
-    expect(node_info.host.class).to be String
-    expect(node_info.host).to eq(node_info_host)
+    expect(@node_info.port.class).to be(Fixnum)
+    expect(@node_info.port).to       eq(@node_info_port)
 
-    expect(node_info.port.class).to be Fixnum
-    expect(node_info.port).to eq(node_info_port)
+    expect(@node_info.status.class).to be(Fixnum)
+    expect(@node_info.status).to       eq(@node_info_status)
 
-    expect(node_info.status.class).to be Fixnum
-    expect(node_info.status).to eq(node_info_status)
-
-    expect(node_info.weight.class).to be Float
-    expect(node_info.weight).to eq(node_info_weight)
+    expect(@node_info.weight.class).to be(Float)
+    expect(@node_info.weight).to       eq(@node_info_weight)
   end
 
   it 'can be converted to a hash' do
-
-    node_info_id     = 1
-    node_info_host   = 'backend1'
-    node_info_port   = 5432
-    node_info_status = 0
-    node_info_weight = 0.5
-    node_info        = PGPool::Wrapper::NodeInfo.new(node_info_id, node_info_host, node_info_port, node_info_status, node_info_weight)
-
-    expect(node_info.to_hash).to eq({ id: node_info_id, host: node_info_host, port: node_info_port, status: node_info_status, weight: node_info_weight })
+    expect(@node_info.to_hash).to eq({ id: @node_info_id, host: @node_info_host, port: @node_info_port, status: @node_info_status, weight: @node_info_weight })
   end
 
   it 'only admits valid status' do
-    expect(PGPool::Wrapper::NodeInfo.new(1, "backend1", 5432, PGPool::Wrapper::NodeInfo::INITIALIZING, 0.5).class).to be PGPool::Wrapper::NodeInfo
-    expect(PGPool::Wrapper::NodeInfo.new(1, "backend1", 5432, PGPool::Wrapper::NodeInfo::UP_NO_CONNECTIONS, 0.5).class).to be PGPool::Wrapper::NodeInfo
-    expect(PGPool::Wrapper::NodeInfo.new(1, "backend1", 5432, PGPool::Wrapper::NodeInfo::UP, 0.5).class).to be PGPool::Wrapper::NodeInfo
-    expect(PGPool::Wrapper::NodeInfo.new(1, "backend1", 5432, PGPool::Wrapper::NodeInfo::DOWN, 0.5).class).to be PGPool::Wrapper::NodeInfo
+    [PGPool::Wrapper::NodeInfo::INITIALIZING, 
+     PGPool::Wrapper::NodeInfo::UP_NO_CONNECTIONS,
+     PGPool::Wrapper::NodeInfo::UP,
+     PGPool::Wrapper::NodeInfo::DOWN].each do |status| 
+      expect(PGPool::Wrapper::NodeInfo.new(1, "backend1", 5432, status, 0.5).class).to be(PGPool::Wrapper::NodeInfo)
+    end
+  end
 
+  it 'does not admin unkown status values' do
     expect { PGPool::Wrapper::NodeInfo.new(1, "backend1", 5432, 4, 0.5) }.to raise_error(RuntimeError)
   end
 
@@ -82,6 +75,13 @@ describe PGPool::Wrapper::NodeInfo do
     expect(PGPool::Wrapper::NodeInfo.new(1, "backend1", 5432, PGPool::Wrapper::NodeInfo::UP_NO_CONNECTIONS, 0.5).initializing?).to eq(false)
     expect(PGPool::Wrapper::NodeInfo.new(1, "backend1", 5432, PGPool::Wrapper::NodeInfo::UP, 0.5).initializing?).to eq(false)
     expect(PGPool::Wrapper::NodeInfo.new(1, "backend1", 5432, PGPool::Wrapper::NodeInfo::DOWN, 0.5).initializing?).to eq(false)
+  end
+
+  it 'mark the node as up without connections' do
+    expect(PGPool::Wrapper::NodeInfo.new(1, "backend1", 5432, PGPool::Wrapper::NodeInfo::INITIALIZING, 0.5).up?).to eq(false)
+    expect(PGPool::Wrapper::NodeInfo.new(1, "backend1", 5432, PGPool::Wrapper::NodeInfo::UP_NO_CONNECTIONS, 0.5).up?).to eq(true)
+    expect(PGPool::Wrapper::NodeInfo.new(1, "backend1", 5432, PGPool::Wrapper::NodeInfo::UP, 0.5).up?).to eq(true)
+    expect(PGPool::Wrapper::NodeInfo.new(1, "backend1", 5432, PGPool::Wrapper::NodeInfo::DOWN, 0.5).up?).to eq(false)
   end
 
   it 'mark the node as up' do
